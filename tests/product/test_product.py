5c86a02d7c50e86a343b85482956f4884390b966
p4n1k0 <gabre.tarick@gmail.com>
2023-03-03 18:11:29 -0300

    teste cria arqv produto com tds atributos

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    p = Product(
+        1,
+        'produto',
+        'empresa',
+        2023,
+        2025,
+        12,
+        'em qualquer lugar',
+    )
+
+    res = (
+        'O produto produto fabricado em 2023 por empresa com'
+        ' validade até 2025 precisa ser armazenado em qualquer lugar.'
+    )
+    assert repr(p) == res
