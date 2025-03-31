766c123e1c056bc9cbd6267d028c0366b774efa7
p4n1k0 <gabre.tarick@gmail.com>
2025-03-30 21:15:06 -0300

    cria classe InventoryIterator + testes + preenche funcao main no modulo inventory_report/main

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        'café',
+        'Minas Cafés',
+        '2023-01-01',
+        '2023-12-12',
+        'cafe123',
+        'em local fresco'
+    )
+
+    assert produto.__repr__() == (
+        "O produto café"
+        " fabricado em 2023-01-01"
+        " por Minas Cafés com validade"
+        " até 2023-12-12"
+        " precisa ser armazenado em local fresco."
+        )
